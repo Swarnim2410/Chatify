972b5ce1c97f02d0e678b676d40d515ee0058a07
Swarnim2410 <swarnimagupta2124@gmail.com>
2024-04-17 21:00:03 +0530

    deployment

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import { app, server } from "./socket/socket.js";
+import path from "path";
 
 //route imports
 import authRoutes from "./routes/auth.routes.js";
@@ -18,6 +19,8 @@ app.use(cookieParser());
 dotenv.config();
 const PORT = process.env.PORT_NUMBER || 3000;
 
+const __dirname = path.resolve();
+
 //connecting database -->
 mongoose
   .connect(process.env.MONGO_URL)
@@ -27,5 +30,11 @@ mongoose
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")))
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
